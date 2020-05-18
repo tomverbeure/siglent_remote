c6dd7c99965d6ec7d1627986e3ba106f55c6552d
Tom Verbeure <hombre-github@gmail.com>
2020-05-18 00:51:43 -0700

    Add vxi11_ident.py to Makefile

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 
 IP_ADDR = 192.168.1.177
 
+id:
+	./vxi11_ident.py $(IP_ADDR)
+
 view:
 	./screenshot.py -o screenshot.png $(IP_ADDR)
 	eog screenshot.png
--- vxi11_ident.py
+++ vxi11_ident.py
@@ -1,8 +1,14 @@
 #! /usr/bin/env python3
 
+import argparse
 import vxi11
 
-instr = vxi11.Instrument("192.168.1.176")
+parser = argparse.ArgumentParser(description='Dump identification number.')
+parser.add_argument('device', metavar='DEVICE', nargs=1, help='the ip address or hostname of the device')
+
+args = parser.parse_args()
+
+instr = vxi11.Instrument(args.device[0])
 print(instr.ask("*IDN?"))
 
 
